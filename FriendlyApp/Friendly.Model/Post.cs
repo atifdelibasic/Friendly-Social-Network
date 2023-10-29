935d21c8b33c0e102f35cbabc03d8d2ee56a32f7
unknown <atif.delibasic@edu.fit.ba>
2023-10-29 14:03:01 +0100

    update model.post properties order

--- FriendlyApp/Friendly.Model/Post.cs
+++ FriendlyApp/Friendly.Model/Post.cs
@@ -5,11 +5,12 @@ namespace Friendly.Model
     {
         public int Id { get; set; }
         public string Description { get; set; }
-        public User User { get; set; }
-        public DateTime DateCreated { get; set; }
-        public Hobby Hobby { get; set; }
         public string ImagePath { get; set; }
+        public DateTime DateCreated { get; set; }
         public int LikeCount { get; set; }
         public int CommentCount { get; set; }
+        public User User { get; set; }
+        public Hobby Hobby { get; set; }
+      
     }
 }
